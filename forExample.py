2e6470787172af4ea86b4c8bd9a6b76947f63997
akkiram <114799941+akkiram@users.noreply.github.com>
2022-10-30 15:43:20 +0530

    python
    firstexmple

---
+++ forExample.py
@@ -0,0 +1,18 @@
+#cars = ["nano", "ford", "tata", "bmw", "thar"]
+
+#for  i in cars:
+ #   if i == "bmw":
+ #       break
+#  print(i)
+
+#for i in  ("ram"):
+ #   print(i)
+
+
+for i in range(6):
+    if i == 3:
+        break
+    print(i)
+else:
+    print("loop over")
+    
